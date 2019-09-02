9e24caad06208415eff4c2db838aac8dbcbe7b8b
Ricardo Forte <rforte@redhat.com>
2019-09-01 22:31:44 -0300

    initial version

---
+++ src/main/java/com/redhat/demo/serasa/GreetingService.java
@@ -0,0 +1,11 @@
+package com.redhat.demo.serasa;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class GreetingService {
+
+    public String greeting(String name) {
+        return "hello " + name;
+    }
+}
